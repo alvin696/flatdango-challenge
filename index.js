363b45f4956815b7fb90dd0cc67636c62ddcd9e7
Alvin kipkirui yaem <alvinyaem4@gmail.com>
2023-04-03 10:26:15 +0300

    Added new feature to index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+const filmsUrl = 'http://localhost:3000/films';
+
+// Load movie details when the page loads
+window.onload = function() {
+  loadMovieDetails(1);
+  loadMovieList();
+};
+
+// Load movie list on the left sidebar
+function loadMovieList() {
+  const filmList = document.getElementById('films');
+  fetch(filmsUrl)
+    .then(response => response.json())
+    .then(data => {
+      filmList.innerHTML = '';
+      for (let i = 0; i < data.length; i++) {
+        const film = data[i];
+        const li = document.createElement('li');
+        li.classList.add('film', 'item');
+        li.innerText = film.title;
+        li.addEventListener('click', function() {
+          loadMovieDetails(film.id);
+        });
+        if (film.capacity - film.tickets_sold === 0) {
+          li.classList.add('sold-out');
+        }
+        filmList.appendChild(li);
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+// Load movie details in the main area
+function loadMovieDetails(id) {
+  const movieTitle = document.getElementById('movie-title');
+  const poster = document.getElementById('poster');
+  const runtime = document.getElementById('runtime');
+  const showtime = document.getElementById('showtime');
+  const availableTickets = document.getElementById('available-tickets');
+  const buyTicketBtn = document.getElementById('buy-ticket-btn');
+  fetch(`${filmsUrl}/${id}`)
+    .then(response => response.json())
+    .then(data => {
+      movieTitle.innerText = data.title;
+      poster.src = data.poster;
+      runtime.innerText = `${data.runtime} min`;
+      showtime.innerText = data.showtime;
+      availableTickets.innerText = data.capacity - data.tickets_sold;
+      if (data.capacity - data.tickets_sold === 0) {
